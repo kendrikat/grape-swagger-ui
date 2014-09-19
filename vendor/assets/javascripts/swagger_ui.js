52d7aa274d750512f43baf980eed8db75fcf25c3
Alex Agranov <alex@morphogenic.net>
2014-09-18 21:03:57 -0400

    updating swagger-ui JS in vendor to v2.0.24

--- vendor/assets/javascripts/swagger_ui.js
+++ vendor/assets/javascripts/swagger_ui.js
@@ -11,13 +11,15 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 
+//= require './lib/shred.bundle.js'
 //= require './lib/jquery-1.8.0.min.js'
 //= require './lib/jquery.slideto.min.js'
 //= require './lib/jquery.wiggle.min.js'
 //= require './lib/jquery.ba-bbq.min.js' 
-//= require './lib/handlebars-1.0.rc.1.js' 
+//= require './lib/handlebars-1.0.0.js' 
 //= require './lib/underscore-min.js' 
 //= require './lib/backbone-min.js' 
 //= require './lib/swagger.js' 
 //= require 'swagger-ui.js' 
 //= require './lib/highlight.7.3.pack.js'
+//= require './lib/swagger-oauth.js'
